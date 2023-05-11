778c871fb384a6b9fb619313907337a749cc6a9a
FurtherV <alex.howel@web.de>
2023-05-11 20:21:13 +0200

    init

---
+++ scripts/constants.mjs
@@ -0,0 +1,4 @@
+export const MODULE_ID = "furtherv-utilities-5e";
+export const MODULE_NAME = "FurtherV's Utilities 5e";
+export const MODULE_LANG_ID = MODULE_ID.toUpperCase();
+export const MODULE_SETTINGS = {};
---
+++ setup.mjs
@@ -0,0 +1,1 @@
+// This file is automatically executed by FoundryVTT
